08744bef9b34c1c0c961842ff45735bb2bea2a8c
Harvey Russell <2461525r@student.gla.ac.uk>
2023-02-18 15:24:16 +0000

    cream_puff solved

--- HackerRank/cream_puff.py
+++ HackerRank/cream_puff.py
@@ -1,9 +1,18 @@
 import sys
+import math
 
 def distribute(n):
-    for i in range(1, n+1):
+    numbers = []
+    if n < 1 or n>10**12:
+        return 
+    for i in range(1, int(math.sqrt(n))+1):
         if n % i == 0:
-            print(i)
+            numbers.append(i)
+            if i != n // i:
+                numbers.append(n // i)
+    numbers.sort()
+    for n in numbers:
+        print(n)
     
 for line in sys.stdin:
     distribute(int(line))
