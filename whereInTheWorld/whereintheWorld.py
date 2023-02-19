66f5f14dc08fdc76ec3a6e0c9e621bc4f7e009b2
Robbert Sinclair <2432710S@student.gla.ac.uk>
2023-02-19 10:51:27 +0000

    Added the available flights

--- whereInTheWorld/whereintheWorld.py
+++ whereInTheWorld/whereintheWorld.py
@@ -13,29 +13,38 @@ def findFlight(flight_number):
             return api.get_flight_details(flight.id)
     return None
 
+api = FlightRadar24API()
+flight_numbers = [flight.number for flight in api.get_flights()]
+lines = []
+index = 0
+print("Available Flights")
+while index < len(flight_numbers):
+    new_line = "\t".join(flight_numbers[index:index+10])
+    index += 10
+    print(new_line)
 
 print("Gimme a plane yo!")
 flight_number = input("Plane Number: ")
 flight_details = findFlight(flight_number)
 #print(flight_details["trail"][0])
-#try:
-coordinates = (flight_details["trail"][0]["lat"], flight_details["trail"][0]["lng"])
-location = geocoder.reverse(coordinates)
-print(f"Origin: {flight_details['airport']['origin']['name']}")
-print(f"Destination: {flight_details['airport']['destination']['name']}")
-print(f"The plane is at coordinates {coordinates}")
+try:
+    coordinates = (flight_details["trail"][0]["lat"], flight_details["trail"][0]["lng"])
+    location = geocoder.reverse(coordinates)
+    print(f"Origin: {flight_details['airport']['origin']['name']}")
+    print(f"Destination: {flight_details['airport']['destination']['name']}")
+    print(f"The plane is at coordinates {coordinates}")
 
-url = f"https://api.what3words.com/v3/convert-to-3wa?coordinates={coordinates[0]},{coordinates[1]}&key=AAY46H1S"
-response = requests.get(url)
-data = response.json()
-if not location:
+    url = f"https://api.what3words.com/v3/convert-to-3wa?coordinates={coordinates[0]},{coordinates[1]}&key=AAY46H1S"
+    response = requests.get(url)
+    data = response.json()
+    if not location:
+        
+        print(f"Nearest location is {data['nearestPlace']}")
+    else:
+        print(location)
+    print(f"What3Words = {data['words']}")
     
-    print(f"Nearest location is {data['nearestPlace']}")
-else:
-    print(location)
-print(f"What3Words = {data['words']}")
     
     
-    
-#except:
-    #print("We couldn't find the plane")
+except:
+    print("We couldn't find the plane")
