eec60f6357b9e2066d293dd7aeed3618c2cc200a
Robbert Sinclair <2432710S@student.gla.ac.uk>
2023-02-19 09:53:55 +0000

    Updated whereInTheWorld

--- whereInTheWorld/whereintheWorld.py
+++ whereInTheWorld/whereintheWorld.py
@@ -24,13 +24,16 @@ location = geocoder.reverse(coordinates)
 print(f"Origin: {flight_details['airport']['origin']['name']}")
 print(f"Destination: {flight_details['airport']['destination']['name']}")
 print(f"The plane is at coordinates {coordinates}")
-print(location)
+
 url = f"https://api.what3words.com/v3/convert-to-3wa?coordinates={coordinates[0]},{coordinates[1]}&key=AAY46H1S"
 response = requests.get(url)
 data = response.json()
 if not location:
-    print(f"What3Words = {data['words']}")
+    
     print(f"Nearest location is {data['nearestPlace']}")
+else:
+    print(location)
+print(f"What3Words = {data['words']}")
     
     
     
