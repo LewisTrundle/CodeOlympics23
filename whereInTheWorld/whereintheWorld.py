39d7e81f5689a0d9462a6b6f027868ab7864482f
Robbert Sinclair <2432710S@student.gla.ac.uk>
2023-02-19 09:48:38 +0000

    Where in the world

---
+++ whereInTheWorld/whereintheWorld.py
@@ -0,0 +1,38 @@
+from FlightRadar24.api import FlightRadar24API
+import time
+from geopy.geocoders import Nominatim
+geocoder = Nominatim(user_agent = 'your_app_name')
+import requests
+import json
+
+def findFlight(flight_number):
+    api = FlightRadar24API()
+    flights = api.get_flights()
+    for flight in flights:
+        if (flight.number == flight_number):
+            return api.get_flight_details(flight.id)
+    return None
+
+
+print("Gimme a plane yo!")
+flight_number = input("Plane Number: ")
+flight_details = findFlight(flight_number)
+#print(flight_details["trail"][0])
+#try:
+coordinates = (flight_details["trail"][0]["lat"], flight_details["trail"][0]["lng"])
+location = geocoder.reverse(coordinates)
+print(f"Origin: {flight_details['airport']['origin']['name']}")
+print(f"Destination: {flight_details['airport']['destination']['name']}")
+print(f"The plane is at coordinates {coordinates}")
+print(location)
+url = f"https://api.what3words.com/v3/convert-to-3wa?coordinates={coordinates[0]},{coordinates[1]}&key=AAY46H1S"
+response = requests.get(url)
+data = response.json()
+if not location:
+    print(f"What3Words = {data['words']}")
+    print(f"Nearest location is {data['nearestPlace']}")
+    
+    
+    
+#except:
+    #print("We couldn't find the plane")
