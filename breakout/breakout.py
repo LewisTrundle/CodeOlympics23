47ef08abb936994f4d8a1c52a29f6d0ddd2d6f86
Ryan Gregson <ryangregson01@outlook.com>
2023-02-19 11:44:49 +0000

    Breakout done

--- breakout/breakout.py
+++ breakout/breakout.py
@@ -1,47 +1,52 @@
 import numpy as np
+import random
 
 friend_lists = {'Chris' : ['Paul','Dave'], 'Paul': ['Chris', 'Dave'], 'Dave':['Paul', 'Chris'],
     'Ryan': ['Harvey'], 'Harvey':['Ryan'], 
     'Lewis':['Connor'], 'Connor':['Lewis']}
 
 
-
-
-
 kk = friend_lists.keys()
-print(kk)
 ids = {}
 for i, name in enumerate(kk):
     ids[name] = i
-
 kk_as_ids = [ids.get(name) for name in kk]
-print(kk_as_ids)
 
 vv = [ np.array([ ids.get(f) for f in friends]) for friends in friend_lists.values()]
-print(vv)
-
 len_vv = [len(v) for v in vv]
-
-print(len_vv)
-
 user_index_w_most_friends = np.flip(np.argsort(len_vv))
 
 no_of_teams = 3
+# How big we want the teams
+avg_len = round((len(kk_as_ids) / no_of_teams))
 teams = [ [] for i in range(no_of_teams) ]
-print(teams)
+
+# Choosing user with friends as they are the most difficult to place
 for val in user_index_w_most_friends:
     user_code = kk_as_ids[val]
-    print(user_code)
-
-    for team in teams:
+    for i, team in enumerate(teams):
         friend_exists = False
         for v in vv[val]:
             if v in team:
                 friend_exists = True
 
-        if not friend_exists:
+        if not friend_exists and (len(team) < avg_len):
             team.append(user_code)
-            print(team)
+            break
 
+        # If user can absolutely not be place, break average sized teams to place them at the end
+        if i == (len(teams)-1):
+            n = random.randint(0, i)
+            teams[n].append(user_code)
 
+# User codes to names
+reversed_dict = {}
+for key, value in ids.items():
+    if value not in reversed_dict:
+        reversed_dict[value] = key
+    else:
+        print("Error")
+
+teams_and_names = [ [reversed_dict.get(name) for name in team] for team in teams]
+print(teams_and_names)
 
